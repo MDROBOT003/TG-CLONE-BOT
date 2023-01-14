3f9491b0ea16c61f1ca41ce527efc4502a5f15cf
MDROBOT003 <120124913+MDROBOT003@users.noreply.github.com>
2023-01-14 10:58:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config(object):
     TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BQCWNWIAKrD5-x1C5_Aa3tlG_gkiFbXiXN22VP6Hi5rm8NZMkoSQtP1H-Q4OOZW98b1niJ4LnDgJy_z5K85kjgcrnYrZX5JGNTAGyVcLQUEPirCIuQjIdgbdWOxVzthfjAab9NN3hHdD3v6480iwvhbeXDWwPidoD36R3lEgmRXxBJp2gTyy4Yx0TaFYW7CSHamvQIhGJTrMiwsYu1YwBdgsNk5aiO8cCgJOUgsuLuBVg8i2l6qXGeLEIpRH23FoKIPJCX_CRX3dmTimbOLg9LAtyemU8cCstpOQvwlyJtm5kZLb3cJEk_WNiVVuxItHmI3WYrLDNsvD1kDrbWyiSlYIGvbxOwAAAABphwJ4AA")
 
     # Database URI
-    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://clone:computer90@cluster0.jff6fof.mongodb.net/?retryWrites=true&w=majority")
+    DB_URI = os.environ.get("DATABASE_URL", "")
 
 
 def LOGGER(name: str) -> logging.Logger:
